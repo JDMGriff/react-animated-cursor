a4d0d17093d19955f15962027026e82bfd7282fe
JDMGriff <griffithsimg@gmail.com>
2025-02-17 10:01:57 +0000

    updated touchdevice logic
    added media query to control touch device logic due to cursor still showing on tap on mobile.

--- lib/hooks/useIsTouchdevice.ts
+++ lib/hooks/useIsTouchdevice.ts
@@ -2,12 +2,16 @@ import { useEffect, useState } from 'react'
 
 const useIsTouchdevice = (): boolean => {
   const [isTouchdevice, setIsTouchdevice] = useState<boolean>()
+  const screenWidth = typeof window !== 'undefined' ? window.innerWidth : 0
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setIsTouchdevice(window.matchMedia('(hover: none)').matches)
     }
-  }, [])
+    if (screenWidth < 768) {
+      setIsTouchdevice(window.matchMedia('(hover: none)').matches)
+    }
+  }, [screenWidth])
 
   return isTouchdevice
 }
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import external from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
+// eslint-disable-next-line prettier/prettier
 import pkg from './package.json' with { type: 'json' }
 
 const umdGlobals = {
